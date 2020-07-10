029075cf7f7cf01e6ba58e184d1c7f5a67dc32b7
gabriel887 <49587014+gabriel887@users.noreply.github.com>
2020-07-09 23:13:10 -0300

    Update all-categoria.php
    corrige uns errinho

--- src/categorias/modelo/all-categoria.php
+++ src/categorias/modelo/all-categoria.php
@@ -5,33 +5,34 @@
     if($conexao){
 
         $sql = "SELECT idcategoria, nome FROM categorias WHERE 1=1 AND ativo like 'S' ";
-        $resultado = mysqli_query($conexao, $sql);
-        $qtdeLinhas = mysqli_num_rows($resultado);
-
-        $resultado = mysqli_query($conexao, $sql);
-        $totalFiltrados = mysqli_num_rows($resultado);
-
-
+        
         $resultado = mysqli_query($conexao, $sql);
 
-        $dados = array();
-        while($linha = mysqli_fetch_assoc($resultado)){
-            $dados[] = array_map('utf8_encode', $linha);
+        if($resultado && mysqli_num_rows($resultado) > 0){
+            while($linha = mysqli_fetch_assoc($resultado)){
+                $categorias[] = array_map('utf8_encode', $linha);
+            }
+            $dados = array(
+                "tipo" => TYPE_MSG_SUCCESS,
+                "mensagem" => '',
+                "dados" => $categorias
+            );
+        }else{
+            $dados = array(
+                "tipo" => TYPE_MSG_ERROR,
+                "mensagem" => "Não foi possível localizar nenhuma categoria",
+                "dados" => array()
+            );
         }
-
-        $json_data = array(
-            "data" => $dados
-        );
 
         mysqli_close($conexao);
 
-    } else{
-        $json_data = array(
-            "draw" => 0,
-            "recordsTotal" => 0,
-            "recordsFiltered" => 0,
-            "data" => array()
+    } else {
+        $dados = array(
+            "tipo" => TYPE_MSG_INFO,
+            "mensagem" => "Não foi possível conectar ao banco de dados",
+            "dados" => array()
         );
     }
 
-    echo json_encode($json_data, JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE);
+    echo json_encode($dados, JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE);
